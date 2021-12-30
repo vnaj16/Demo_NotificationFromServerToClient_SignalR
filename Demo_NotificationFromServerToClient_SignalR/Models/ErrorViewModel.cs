9129c62edd847ecfaa249f7bdc3cf441098b8f33
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2021-12-30 10:14:36 -0500

    Initial commit

---
+++ Demo_NotificationFromServerToClient_SignalR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Demo_NotificationFromServerToClient_SignalR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
